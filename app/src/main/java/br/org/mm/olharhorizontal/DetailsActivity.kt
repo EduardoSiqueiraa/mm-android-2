d078195a69ff9311c435f5e7d2ee0536d697f354
FL-SS-204-15\Aluno <leonam.murilo@gmail.com>
2018-04-21 12:09:16 -0300

    Add details screen layout

---
+++ app/src/main/java/br/org/mm/olharhorizontal/DetailsActivity.kt
@@ -0,0 +1,15 @@
+package br.org.mm.olharhorizontal
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Aluno on 21/04/2018.
+ */
+class DetailsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
